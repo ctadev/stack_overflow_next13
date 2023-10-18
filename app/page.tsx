5e014c13143cf64b250c45dbcc282890f5afe897
ctadev <ctakammy@gmail.com>
2023-10-18 10:04:38 -0400

    clerk auth implementation

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="h1-bold">NextJ13 WebOverflow!</h1>
-      <h2 className="h2-bold">NextJ13 WebOverflow!</h2>
-      <h3 className="h3-bold">NextJ13 WebOverflow!</h3>
-    </main>
-  );
-}
